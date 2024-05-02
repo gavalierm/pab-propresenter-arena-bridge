43dfefeeb4c595f95855b022799f9c822c9130ae
gavalierm <marcel@gavalier.sk>
2024-05-02 19:00:09 +0200

    upgrade

--- node_modules/gun/sea/root.js
+++ node_modules/gun/sea/root.js
@@ -4,6 +4,7 @@
     // IT IS IMPLEMENTED IN A POLYFILL/SHIM APPROACH.
     // THIS IS AN EARLY ALPHA!
 
+    if(typeof self !== "undefined"){ module.window = self } // should be safe for at least browser/worker/nodejs, need to check other envs like RN etc.
     if(typeof window !== "undefined"){ module.window = window }
 
     var tmp = module.window || module, u;
--- node_modules/gun/sea/then.js
+++ node_modules/gun/sea/then.js
@@ -1,5 +1,5 @@
 
-    var u, Gun = (''+u != typeof window)? (window.Gun||{chain:{}}) : require((''+u === typeof MODULE?'.':'')+'./gun', 1);
+    var u, Gun = (''+u != typeof GUN)? (GUN||{chain:{}}) : require((''+u === typeof MODULE?'.':'')+'./gun', 1);
     Gun.chain.then = function(cb, opt){
       var gun = this, p = (new Promise(function(res, rej){
         gun.once(res, opt);
